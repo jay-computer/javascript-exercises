c63aa136063b337150b6df19308e68bf4d454d27
jay-computer <joshuakang19@gmail.com>
2022-06-16 10:56:12 -0400

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(inp) {
+    inp = parseInt(inp);
+    if(inp <= 0) {
+        return "OOPS";
+    }
+    if(inp === 1) {
+        return 1;
+    }
+    if(inp === 2) {
+        return 1;
+    }
 
+    let fibboo = [1, 1];
+    for(let i = 2; i < inp; i = i +1) {
+        fibboo[i] = fibboo[i-1] + fibboo[i-2];
+    }
+    return fibboo[inp-1];
 };
 
 // Do not edit below this line
