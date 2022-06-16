bbc81f2446d5b1c95e14f9c5875fcb9023ec21b8
jay-computer <joshuakang19@gmail.com>
2022-06-16 11:25:00 -0400

    Finish Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest;
+    let age = 0;
+    for(person of people) {
+        let death = person["yearOfDeath"];
 
+        if(typeof person["yearOfDeath"] === "undefined") {
+            death = 2022;
+        }
+        let currentAge = death - person["yearOfBirth"];
+        if(currentAge > age) {
+            age = currentAge;
+            oldest = person;
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
