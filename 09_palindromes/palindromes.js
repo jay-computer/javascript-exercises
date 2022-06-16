d5131d941d888f90815b4a66b8f3a32e2c9abb70
jay-computer <joshuakang19@gmail.com>
2022-06-16 10:48:53 -0400

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
-
+const palindromes = function (inpSentence) {
+    inpSentence = inpSentence.toLowerCase();
+    let letList = inpSentence.split("");
+    let ans = new Array();
+    
+    
+    for (letty of letList) {
+        if(isLetter(letty) === true) {
+            ans.push(letty);
+        }
+    }
+    
+    let threshold = Math.floor(ans.length / 2);
+    for(let i = 0; i < threshold; i = i + 1) {
+        if(ans[i] !== ans[ans.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
+
+function isLetter(c) {
+    return c.toLowerCase() != c.toUpperCase();
+}
+
 
 // Do not edit below this line
 module.exports = palindromes;
