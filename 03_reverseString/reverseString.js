f598dd7f5521591a5e676181c5f3c08c1783ec64
jay-computer <joshuakang19@gmail.com>
2022-06-15 21:43:35 -0400

    Finish Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let splitted = input.split("");
+    let ans = new Array();
+    for(let i = splitted.length; i > -1; i = i - 1) {
+        ans.push(splitted[i]);
+        
+    }
+    return ans.join('');
 };
 
 // Do not edit below this line
