46c141e9e9993beb323fffd99f107847ffbdf9d0
jay-computer <joshuakang19@gmail.com>
2022-06-15 21:30:04 -0400

    Finish exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(repeated, iterations) {
+    if(iterations < 0) {
+        return "ERROR";
+    }
+    let ret = "";
+    for (let i = 0; i < iterations; i++) {
+        ret = ret + repeated;
+    }
+    return ret;
 };
 
 // Do not edit below this line
