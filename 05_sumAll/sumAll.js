f6c63e2adf53bee245d19abffd5a30bb927e3a28
jay-computer <joshuakang19@gmail.com>
2022-06-15 22:11:23 -0400

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    let sum = 0;
+    if((typeof(first) != "number") || (typeof(second) != "number")) return "ERROR";
+    if(first < 0 || second < 0) return "ERROR";
+    let lowerBound = (first < second) ? first : second;
+    let upperBound = (first < second) ? second : first;
+    for(let i = lowerBound; i <= upperBound; i = i + 1) {
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
