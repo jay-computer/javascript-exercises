ee042a12191d5c3a22f288e0231efbcb8947740b
jay-computer <joshuakang19@gmail.com>
2022-06-16 09:08:40 -0400

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numList) {
+  let tot = 0;
+	for(num of numList) {
+    tot = tot + num;
+  }
+  return tot;
 };
 
-const multiply = function() {
-
+const multiply = function(numList) {
+  let tot = 1;
+	for(num of numList) {
+    tot = tot * num;
+  }
+  return tot;
 };
 
-const power = function() {
-	
+const power = function(base, expo) {
+	return Math.pow(base, expo);
 };
 
-const factorial = function() {
-	
+const factorial = function(base) {
+	if(base === 0 || base ===1) {
+    return 1;
+  }
+
+  let tot = 1;
+  for (let i = base; i > 0; i = i - 1) {
+    tot = tot * i;
+  }
+  return tot;
 };
 
 // Do not edit below this line
