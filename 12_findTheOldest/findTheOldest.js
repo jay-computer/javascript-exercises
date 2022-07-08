a4467c712acce7aba1b892640c017cc812cab2a2
jay-computer <joshuakang19@gmail.com>
2022-07-08 16:13:32 -0400

    Convert exercises into array methods

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,5 @@
 const findTheOldest = function(people) {
+    /*
     let oldest;
     let age = 0;
     for(person of people) {
@@ -14,6 +15,28 @@ const findTheOldest = function(people) {
         }
     }
     return oldest;
+    */
+   const oldest = people.sort((a, b) => {
+    let aDeath, bDeath;
+        if(typeof a["yearOfDeath"]==="undefined") {
+            aDeath = 2022;
+        }
+        else {
+            aDeath = a["yearOfDeath"];
+        }
+        if(typeof b["yearOfDeath"]==="undefined") {
+            bDeath = 2022;
+        }
+        else {
+            bDeath = b["yearOfDeath"];
+        }
+    
+    let aAge = aDeath - a["yearOfBirth"];
+    let bAge = bDeath - b["yearOfBirth"];
+
+    return (aAge > bAge) ? -1 : 1;
+   });
+   return oldest[0];
 };
 
 // Do not edit below this line
